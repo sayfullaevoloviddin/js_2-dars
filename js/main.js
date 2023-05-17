1af40355992ed166adc4e464298d676fe991dd63
sayfullaevoloviddin <sayfullaevoloviddin73@gmail.com>
2023-05-17 22:12:58 +0500

    js 2-dars yuklandi
    js 2-darsning kodlari

---
+++ js/main.js
@@ -0,0 +1,68 @@
+/*
+if (agar)
+else (u holda)
+else if (yoki bo'lmasa)
+*/
+/*let x = 100;*/
+/*let y = 5;*/
+/*if( x == y ){
+    document.write( x + y);
+}else{
+    document.write( x - y);
+}*/
+/*
+if( x == y){
+    document.write( x * y);
+}else{
+    document.write( x / y);
+}
+*/
+/*
+if( 'emal' == 'email'){
+    document.write("Email to'g'ri");
+}
+else if( 45683833 == 45683833){
+    document.write("Parol to'g'ri");
+}
+else{
+    document.write("Email va parol noto'g'ri! yana bir bor urinib koring");
+}
+*/
+/*
+if("45" > "54"){
+    document.write("1-solishtirish bajarildi!");
+}
+else if(68 < 98){
+    document.write("2-solishtirish bajarildi!");
+}
+else{
+    document.write("Yuqoridagi solishtirishlar bajarildi!")
+}
+*/
+/*
+let a = "ism";
+let b = "familiya";
+let c = "email";
+let d = "parol";
+if(a == "ism" && b == "familiya" && c == "email" && d == "parol"){
+    document.write("Ro'yxatdan o'tdingiz!"/!*&& - shartlarning 1tasi noto'g'ri bo'lsa ham shartlar bajarilmaydi*!/);
+}
+else{
+    document.write("Ro'yxatdan o'tmadingiz, ma'lumotlarni to'g'ri kirgizing!");
+}
+*/
+/*
+if(  1230 > 934 || 5242 < 423 || 523 > 4234 || 952 > 32542 ){
+    document.write("Shartlar bajarildi !" /!*|| - shartlarning 1tasi to'gri bo'lsa ham shartlar bajariladi*!/);
+}
+else{
+    document.write("Shartlarning hech biri bajarilmadi!");
+}
+*/
+/* JS data types: undefined, boolen, objeact*/
+/*
+let rost = false;
+document.write(typeof rost)
+*/
+/*let rost = 45 == 44 ? true:false;
+document.write(rost)*/
---
+++ js/main2.js
@@ -0,0 +1,20 @@
+let x = 10 < 5
+if(x===true){
+    document.write("true")
+} else if(x===false){
+    document.write("false")
+} else {
+    document.write("false")
+}
+
+
+
+let y = 485 < 342
+if (y===false){
+    document.write("false")
+} else if(y===true){
+    document.write("true")
+} else {
+    document.write("true")
+}
+
